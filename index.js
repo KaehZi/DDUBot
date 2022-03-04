70cef4261b9f9800407d7a1bdb61aa2bef57d6e1
KaehZi <90711479+KaehZi@users.noreply.github.com>
2022-03-04 10:26:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require("discord.js");
+//const { token } = require("./config.json");
+require("dotenv").config();
+
+// Create a new client instance
+
+const myIntents = new Intents();
+    myIntents.add( Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES )
+const client = new Client({ intents: myIntents });
+// When the client is ready, run this code (only once)
+// client.once("ready", () => {
+//   console.log("I am fucking ready!");
+//});
+client.on('ready', readyDiscord);
+
+function readyDiscord(){
+  console.log('Discord is ready'); 
+  
+}
+client.on("messageCreate",gotMessage);
+
+function gotMessage(msg){
+    //console.log(msg.content);
+
+    let commands = msg.content.split(" ")
+    console.log(commands)
+    if(commands[0] === "!hej" && commands[1] === "med" && commands[2] === "dig"){
+
+        msg.channel.send("Hej med dig!")
+
+    } else if(commands[0] === "!hej" && commands[1] === "bot"){
+
+       msg.channel.send("Hej *biip boop*!")
+    
+    } else if(commands[0] === "!hej"){
+
+        msg.channel.send("Hej!")
+    
+    }
+
+
+
+
+
+    for(let i = 0; i < commands.length; i++){
+    console.log(commands[i]);
+    }
+}
+
+// Login to Discord with your client's token
+client.login();
