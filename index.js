4ab7510a5b111e66e53e45150207ae20afaac4e6
Frederik Kjærgaard Christensen <kcfrede@gmail.com>
2022-03-07 13:21:23 +0100

    død

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//import "./commands"
+//Hello();
 // Require the necessary discord.js classes
 const { Client, Intents } = require("discord.js");
-const { MessageEmbed } = require("discord.js");
-const { SlashCommandBuilder } = require('@discordjs/builders');
+//const { MessageEmbed } = require("discord.js");
+//const { SlashCommandBuilder } = require('@discordjs/builders');
 require("dotenv").config();
 
 // Create a new client instance
@@ -13,7 +15,7 @@ const client = new Client({ intents: myIntents });
 client.on('ready', readyDiscord);
 
 function readyDiscord(){
-  console.log('Discord is ready'); 
+  console.log('Botten er tændt'); 
   
 }
 client.on("messageCreate",gotMessage);
@@ -24,65 +26,68 @@ function gotMessage(msg){
     let commands = msg.content.split(" ")
     console.log(commands)
     //Test af liv for botten med tre simple kommandoer
-    if(commands[0] === "!hej" && commands[1] === "med" && commands[2] === "dig"){
+    // if(commands[0] === "!hej" && commands[1] === "med" && commands[2] === "dig"){
 
-        msg.channel.send("Hej med dig!")
+    //     msg.channel.send("Hej med dig!")
 
-    } else if(commands[0] === "!hej" && commands[1] === "bot"){
+    // } else if(commands[0] === "!hej" && commands[1] === "bot"){
 
-       msg.channel.send("Hej *biip boop*!")
+    //    msg.channel.send("Hej *biip boop*!")
     
-    } else if(commands[0] === "!hej"){
+    // } else if(commands[0] === "!hej"){
 
-        msg.channel.send("Hej!")
+    //     msg.channel.send("Hej!")
     
-    }
+    // }
 
-    if(commands[0] === "madias" && commands[1] === "er" && commands[2] === "gay"){
+    // if(commands[0] === "madias" && commands[1] === "er" && commands[2] === "gay"){
 
-        msg.channel.send("@madias#0744 er gay")
+    //     msg.channel.send("@madias#0744 er gay")
 
-    } else if(commands[0] === "!Madias" && commands[1] === "gay"){
+    // } else if(commands[0] === "!Madias" && commands[1] === "gay"){
 
-       msg.channel.send("*FEEEED*!")
+    //    msg.channel.send("*FEEEED*!")
     
-    } else if(commands[0] === "!gay"){
+    // } else if(commands[0] === "!gay"){
 
-        msg.channel.send("Lasse er sej")
+    //     msg.channel.send("Lasse er sej")
     
-    }
+    // }
     //Pythagoras funktion med embed implementeret
-    if(commands[0] === "/pythagoras"){
+    // if(commands[0] === "/pythagoras"){
 
-        let pythagoras = Math.sqrt(commands[1] * commands[1] + commands[2] * commands[2])
+    //     let pythagoras = Math.sqrt(commands[1] * commands[1] + commands[2] * commands[2])
 
-        //msg.channel.send("Svar: " + pythagoras);
-        const pythagorasEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(commands[0])
-        .setURL('https://discord.js.org/')
-        .setAuthor({ name: 'Studiehjælperen: Udregning af Pythagoras'})
-        .setDescription('Pythagoras udregning af Lone')
-        //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-        .addFields(
-            { name: 'Pythagoras:', value: "Svar: " + pythagoras },
-            //{ name: '\u200B', value: '\u200B' },
-            // { name: 'Inline field title', value: 'Some value here', inline: true },
-            // { name: 'Inline field title', value: 'Some value here', inline: true },
-        )
-        //.addField('Inline field title', 'Some value here', true)
-        //.setImage('https://i.imgur.com/pn9c0BL.jpeg')
-        .setTimestamp()
-        //.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+    //     //msg.channel.send("Svar: " + pythagoras);
+    //     //pythagorasSlash = SlashCommandBuilder().setName('pythagoras').setDescription('Udregner pythagoras'),
+    //     const pythagorasEmbed = new MessageEmbed()
+    //     .setColor('#0099ff')
+    //     .setTitle(commands[0])
+    //     .setURL('https://discord.js.org/')
+    //     .setAuthor({ name: 'Studiehjælperen: Udregning af Pythagoras'})
+    //     .setDescription('Pythagoras udregning af Lone')
+    //     //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+    //     .addFields(
+    //         { name: 'For at udregne med pythagoras, skriv:', value: "/pythagoras <a> <b>"},
+    //         //{ name: '\u200B', value: '\u200B' },
+    //         // { name: 'Inline field title', value: 'Some value here', inline: true },
+    //         // { name: 'Inline field title', value: 'Some value here', inline: true },
+    //     )
+    //     //.addField('Inline field title', 'Some value here', true)
+    //     //.setImage('https://i.imgur.com/pn9c0BL.jpeg')
+    //     .setTimestamp()
+    //     //.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
-        msg.author.send({ embeds: [pythagorasEmbed] });
+    //     msg.channel.send({ embeds: [pythagorasEmbed] });
+    //     }
 
-    }
+    
 
     for(let i = 0; i < commands.length; i++){
     console.log(commands[i]);
     }
-}
+
 
 // Login to Discord with your client's token
 client.login();
+}
