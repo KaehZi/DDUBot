9f62aa18a493b07fcaa54a3d864d05508bbb733f
Frederik Kjærgaard Christensen <kcfrede@gmail.com>
2022-03-07 14:43:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,20 +55,47 @@ function gotMessage(msg){
     // }
     //Pythagoras funktion med embed implementeret
     if(commands[0] === "/pythagoras"){
+      if(isNaN(commands[1])|| isNaN(commands[2])|| isNaN(commands[1]) && isNaN(commands[2])){
+        //let pythagoras = Math.sqrt(commands[1] * commands[1] + commands[2] * commands[2])
 
-        let pythagoras = Math.sqrt(commands[1] * commands[1] + commands[2] * commands[2])
+        //msg.channel.send("Svar: " + pythagoras);
+        //pythagorasSlash = SlashCommandBuilder().setName('pythagoras').setDescription('Udregner pythagoras'),
+        const pythagorasEmbed = new MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(commands[0])
+          .setURL('https://www.webmatematik.dk/lektioner/matematik-c/trigonometri/retvinklede-trekanter')
+          .setAuthor({ name: 'Studiehjælperen: Udregning af Pythagoras'})
+          .setDescription('Pythagoras udregning af Lone')
+          //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+          .addFields(
+              { name: 'For at udregne med pythagoras, skriv:', value: "/pythagoras <a> <b>"},
+            //{ name: '\u200B', value: '\u200B' },
+            // { name: 'Inline field title', value: 'Some value here', inline: true },
+            // { name: 'Inline field title', value: 'Some value here', inline: true },
+          )
+          //.addField('Inline field title', 'Some value here', true)
+          //.setImage('https://i.imgur.com/pn9c0BL.jpeg')
+          .setTimestamp()
+          //.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+        msg.channel.send({ embeds: [pythagorasEmbed] });
+      
+        }else{
+          console.log("noget")
+
+          let pythagoras = Math.sqrt(commands[1] * commands[1] + commands[2] * commands[2])
 
         //msg.channel.send("Svar: " + pythagoras);
         //pythagorasSlash = SlashCommandBuilder().setName('pythagoras').setDescription('Udregner pythagoras'),
         const pythagorasEmbed = new MessageEmbed()
         .setColor('#0099ff')
         .setTitle(commands[0])
-        .setURL('https://discord.js.org/')
+        .setURL('https://www.webmatematik.dk/lektioner/matematik-c/trigonometri/retvinklede-trekanter')
         .setAuthor({ name: 'Studiehjælperen: Udregning af Pythagoras'})
         .setDescription('Pythagoras udregning af Lone')
         //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
         .addFields(
-            { name: 'For at udregne med pythagoras, skriv:', value: "/pythagoras <a> <b>"},
+            { name: 'Pythagoras udregnet, skriv:', value: "Svar: " + pythagoras},
             //{ name: '\u200B', value: '\u200B' },
             // { name: 'Inline field title', value: 'Some value here', inline: true },
             // { name: 'Inline field title', value: 'Some value here', inline: true },
@@ -79,7 +106,11 @@ function gotMessage(msg){
         //.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
         msg.channel.send({ embeds: [pythagorasEmbed] });
+
+
+
         }
+    }
 
     
 
