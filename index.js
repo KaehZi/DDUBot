b14d64db2ca6c9ec29fac3443e869f01d8534c13
Frederik Kjærgaard Christensen <kcfrede@gmail.com>
2022-03-22 20:43:09 +0100

    Slet rumfang hjælp embed

--- index.js
+++ index.js
@@ -437,30 +437,7 @@ function gotMessage(msg){
 
     }
 
-    //Rumfang hjælp kommando
-        if(commands[0] === "/rumfang"){
-            const rumfangHelpEmbed = new MessageEmbed()
-                .setColor('#ffff00')
-                .setTitle("Rumfang beregning")
-                .setURL('https://www.webmatematik.dk/lektioner/7-9-klasse/rumfang-og-overfladeareal')
-                .setAuthor({ name: 'Studiehjælperen: Udregning af rumfang'})
-                .setDescription('Rumfangs beregning af Studiehjælperen')
-                //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-                .addFields(
-                    { name: 'For at udregne rumfang af en firkant/kasse, skriv:', value: "/rumfang firkant <l> <b> <h>"},
-                    { name: 'For at udregne rumfang af en cylinder, skriv:', value: '/rumfang cylinder <h> <r>' },
-                    { name: 'For at udregne rumfang af en kugle, skriv:', value: "/rumfang kugle <r>" },
-                    { name: 'For at udregne rumfang af en kegle, skriv:', value: "/rumfang kegle <h> <r>" },
-                    { name: 'For at udregne rumfang af en pyramide, skriv:', value: '/rumfang pyramide <Areal af grundfladen> <h>'},
-
-                )
-                //.addField('Inline field title', 'Some value here', true)
-                //.setImage('https://i.imgur.com/pn9c0BL.jpeg')
-                .setTimestamp()
-                //.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-    msg.channel.send({ embeds: [rumfangHelpEmbed] });
-        console.log("Rumfang hjælp embed er blevet afsendt")
-    }
+   
     
 
 
