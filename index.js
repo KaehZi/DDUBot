356c3260882ea70cc1f4a90a601fb5c4f06c5952
Frederik Kjærgaard Christensen <kcfrede@gmail.com>
2022-04-08 11:45:51 +0200

    Rettelse af embed og videre bug test uden resultat

--- index.js
+++ index.js
@@ -728,11 +728,23 @@ function gotMessage(msg){
   
             let rumfangPyramide = (commands[2] * commands[3])/3
   
-            
+            const rumfangPyramideEmbed = new MessageEmbed()
+              .setColor('00ff00')
+              .setTitle("Rumfang af en pyramide")
+              .setURL('https://www.webmatematik.dk/lektioner/7-9-klasse/rumfang-og-overfladeareal/pyramide')
+              .setAuthor({ name: 'Studiehjælperen: Udregning af rumfang for et pyramide'})
+              .setDescription('Formel: (Grundfladeareal * h)/3')
+              .addFields(
+                  { name: 'Rumfanget af et pyramide: ', value: "Svar: " + rumfangPyramide},    
+              )
+              .setTimestamp()
+              //.setFooter( {text: ${user} } )
           msg.reply(
             { embeds: [rumfangPyramideEmbed]});
           }
         }  
+
+          
         
         //Ligningsløser
         if(commands[0] === "/ligning"){
@@ -745,23 +757,20 @@ function gotMessage(msg){
               ).join('\n');
               return `<h2>${pod.title}</h2>\n${subpodContent}`;
             }).join('\n');
+
               let solution = output.split("<h2>")
-              //console.log(solution [solution.length-1]);
 
               let solution1 = String(solution);
-              //console.log(solution1);
 
               let solution2 = solution1.split("alt=");
               
               let solution3 = String(solution2)
 
               let solution4 = solution3.split(",")
-            
               
               let solution5 = solution4[solution4.length-1]
             
               let solution6 = solution5.split('"')
-              console.log(solution6)
 
               let solution7 = solution6[1]
 
@@ -775,6 +784,7 @@ function gotMessage(msg){
             { name: 'Løsning af ligningen: ', value: "Svar: " + solution7},    
         )
         .setTimestamp()
+        .setFooter({ text: 'Wolfram|Alpha'});
     msg.reply(
       { embeds: [ligningLøst]});
     })}}
